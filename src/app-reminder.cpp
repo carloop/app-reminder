d9e8f4af36373fd8e74bcf896c84925314fab1c1
Julien Vanier <jvanier@gmail.com>
2016-12-14 23:04:34 -0500

    First compiling version

--- src/app-reminder.cpp
+++ src/app-reminder.cpp
@@ -18,6 +18,10 @@ int changeIntervalLimit(String arg);
 void updateSpeed();
 void requestVehicleSpeed();
 void waitForVehicleSpeedResponse();
+void updateMileage(uint8_t newVehicleSpeedKhm);
+double computeDeltaMileage(uint8_t newVehicleSpeedKhm);
+void checkIntervalLimit();
+void storeMileage();
 
 // Don't block the main program while connecting to WiFi/cellular.
 // This way the main program runs on the Carloop even outside of WiFi range.
@@ -31,24 +35,30 @@ Carloop<CarloopRevision2> carloop;
 Data data;
 // The class that will read and write the data to EEPROM
 Storage storage;
+// Only store to EEPROM every so often
+const auto STORAGE_PERIOD = 60 * 1000; /* every minute */
+uint32_t lastStorageTime = 0;
 
 // OBD constants for CAN
 
 // CAN IDs for OBD messages
-const auto OBD_CAN_REQUEST_ID      = 0x7E0;
-const auto OBD_CAN_REPLY_ID_MIN    = 0x7E8;
+const auto OBD_CAN_REQUEST_ID    = 0x7E0;
+const auto OBD_CAN_REPLY_ID      = 0x7E8;
 
 // Modes (aka services) for OBD
-const auto OBD_MODE_CURRENT_DATA   = 0x01;
+const auto OBD_MODE_CURRENT_DATA = 0x01;
 
 // OBD signals (aka PID) that can be requested
-const auto OBD_PID_VEHICLE_SPEED   = 0x0d;
+const auto OBD_PID_VEHICLE_SPEED = 0x0d;
 
 // Time to wait for a reply for an OBD request
 const auto OBD_TIMEOUT_MS = 10;
 
 uint8_t vehicleSpeedKmh = 0;
+uint32_t lastVehicleSpeedUpdateTime = 0;
 
+// Called at boot
+// Sets up the CAN bus and cloud functions
 void setup()
 {
   setupCloud();
@@ -62,6 +72,7 @@ void setup()
   carloop.begin();
 }
 
+// Allow interacting with the Carloop remotely
 void setupCloud()
 {
   Particle.function("reset", resetIntervalCounter);
@@ -69,6 +80,7 @@ void setupCloud()
   Particle.variable("count", data.intervalCounter);
 }
 
+// Reset the interval counter and store the zero value in EEPROM
 int resetIntervalCounter(String)
 {
   data.intervalCounter = 0;
@@ -77,6 +89,8 @@ int resetIntervalCounter(String)
   return 0;
 }
 
+// Set the interval upper limit and make sure the current value is below
+// that. Store the values value in EEPROM
 int changeIntervalLimit(String arg)
 {
   long newLimit = arg.toInt();
@@ -86,19 +100,20 @@ int changeIntervalLimit(String arg)
   }
 
   data.intervalLimit = newLimit;
-  while (data.intervalCounter >= data.intervalLimit)
-  {
-    data.intervalCounter -= data.intervalLimit;
-    data.intervalReached = 1;
-  }
+  checkIntervalLimit();
   storage.store(data);
   return 0;
 }
 
+// Called over and over
+// Process new CAN messages here to update the vehicle speed, update
+// the mileage and update the interval counter
 void loop()
 {
   updateSpeed();
+  storeMileage();
   publishEvent();
+  delay(100);
 }
 
 void updateSpeed()
@@ -130,7 +145,81 @@ void waitForVehicleSpeedResponse()
   bool received = false;
   while (millis() - start < OBD_TIMEOUT_MS)
   {
+    CANMessage message;
+    if (carloop.can().receive(message))
+    {
+      if (message.id == OBD_CAN_REPLY_ID &&
+          message.data[2] == OBD_PID_VEHICLE_SPEED)
+      {
+        received = true;
+        uint8_t newVehicleSpeedKhm = message.data[3];
+        updateMileage(newVehicleSpeedKhm);
+      }
+    }
+  }
 
+  if (!received)
+  {
+    updateMileage(0);
+  }
+}
+
+void updateMileage(uint8_t newVehicleSpeedKhm)
+{
+  double deltaMileage = computeDeltaMileage(newVehicleSpeedKhm);
+
+  data.intervalCounter += deltaMileage;
+  checkIntervalLimit();
+}
+
+double computeDeltaMileage(uint8_t newVehicleSpeedKhm)
+{
+  uint32_t now = millis();
+  double deltaMileage = 0.0;
+
+  // If the speed was previously 0 or newly 0, or timed out because the
+  // car was off, just save the new speed value
+  if (vehicleSpeedKmh > 0 && newVehicleSpeedKhm > 0)
+  {
+    // The car was previously driving and is still driving
+
+    // Figure out the distance driven using the trapezoidal rule
+    uint32_t msDiff = now - lastVehicleSpeedUpdateTime;
+    // Calculate only if the difference is plausible
+    if (msDiff < 1000) {
+      // distance in km/h * ms
+      uint32_t deltaDistance = msDiff * (vehicleSpeedKmh + newVehicleSpeedKhm) / 2;
+
+      // Convert to miles
+      // 1 kilometer per hour * ms = 1.72603109 × 10^-7 miles
+      // km/h * ms * (1 h / 3600 s) * (0.621371 mi / 1 km) * (1 s / 1000 ms)
+      deltaMileage = deltaDistance * 1.72603109e-7;
+    }
+  }
+
+  vehicleSpeedKmh = newVehicleSpeedKhm;
+  lastVehicleSpeedUpdateTime = now;
+  return deltaMileage;
+}
+
+// If the interval limit is reached, mark it so we can publish an event
+// when we come back to network range
+void checkIntervalLimit()
+{
+  while (data.intervalCounter >= data.intervalLimit)
+  {
+    data.intervalCounter -= data.intervalLimit;
+    data.intervalReached = 1;
+  }
+}
+
+// Store data to EEPROM every so often
+void storeMileage()
+{
+  if (millis() - lastStorageTime > STORAGE_PERIOD)
+  {
+    storage.store(data);
+    lastStorageTime = millis();
   }
 }
 
@@ -142,6 +231,7 @@ void publishEvent()
   {
     // Publish the event interval with the value set to the mileage that was reached
     Particle.publish("interval", String::format("%.0f", data.intervalLimit), PRIVATE);
+    // Clear the flag avoid publishing the same event twice
     data.intervalReached = 0;
     storage.store(data);
   }
